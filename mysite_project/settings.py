522a81be9e67273b1bdcb9db9ea7c916092dfac8
Rafal Matyjasz <r.matyjasz@o2.pl>
2022-11-23 14:21:54 +0100

    add ip address

--- mysite_project/settings.py
+++ mysite_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)arub!ivsxqqc$t$jwj5fyk4m=u$_&u0+i@mk!$wt^%r9puhtw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ec2_metadata.public_hostname]
+ALLOWED_HOSTS = [ec2_metadata.public_hostname, ec2_metadata.public_ipv4]
 
 
 # Application definition
